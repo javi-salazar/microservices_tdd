6ca3500db9fe25b6a544684b3c0312a0fbf73ea7
Javier Salazar <javizz.hjsg@gmail.com>
2019-04-27 19:03:13 -0700

    Add tests for app config, install flask testing

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,14 +1,28 @@
+import sys
+import unittest
+
 from flask.cli import FlaskGroup
-from project import app
+from project import app, db
 
 cli = FlaskGroup(app)
 
 
-@cli.command('recreate_db'):
+@cli.command('recreate_db')
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
 
 if __name__ == '__main__':
     cli()
