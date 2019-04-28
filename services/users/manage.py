207d20c1a5e3cd02333b98fac7cffdab19502732
Javier Salazar <javizz.hjsg@gmail.com>
2019-04-28 14:12:58 -0700

    Flask blueprint setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,9 +2,12 @@ import sys
 import unittest
 
 from flask.cli import FlaskGroup
-from project import app, db
 
-cli = FlaskGroup(app)
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command('recreate_db')
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,41 +1,32 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Instatiate the app
-app = Flask(__name__)
-
-api = Api(app)
-
-# Set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
 
 # Instantiate the DB
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# Model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
+    # Instatiate the app
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # Set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-api.add_resource(UsersPing, '/users/ping')
+    return app
 
