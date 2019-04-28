101fd425fffc6dbda176a2e895c7f3f661de01fc
Javier Salazar <javizz.hjsg@gmail.com>
2019-04-27 18:38:28 -0700

    Add DB setup, postgres, SQLAlchemy, psycopg2

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,5 +3,12 @@ from project import app
 
 cli = FlaskGroup(app)
 
+
+@cli.command('recreate_db'):
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 if __name__ == '__main__':
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # Instatiate the app
 app = Flask(__name__)
@@ -10,6 +11,22 @@ api = Api(app)
 # Set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# Instantiate the DB
+db = SQLAlchemy(app)
+
+
+# Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
