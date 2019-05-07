2ff2cf1e4bcd9907cceca9782ca5f2c0ca86b93b
Javier Salazar <j.salazarg@protonmail.com>
2019-05-06 19:13:06 -0700

    Add React, flask-cors

---
+++ services/client/src/components/UserList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+/**
+ * Functional Component
+ * 
+ * Functional components only render data that is passed in with props or state.
+ * The data flows down, it's read only.
+ */
+
+
+const UserList = (props) => {
+  return (
+    <div>
+      {
+        props.users.map((user) => {
+          return (
+            <h4
+              key={user.id}
+              className="box title is-4"
+            >{ user.username }
+            </h4>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default UserList;
